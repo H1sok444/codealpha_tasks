5e007ebb242cba8ad576df868bc919edbfdb68ad
H1sok4 <88634883+H1sok444@users.noreply.github.com>
2025-03-29 17:08:51 +0000

    Update basic_network_sniffer.py

--- basic_network_sniffer.py
+++ basic_network_sniffer.py
@@ -4,8 +4,11 @@ from scapy.layers.http import HTTPRequest
 
 def get_interface():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interface", dest="interface", help="Specify interface on which to sniff packets ", required=True)
+    parser.add_argument("-i", "--interface", dest="interface", help="Specify interface on which to sniff packets")
     arguments = parser.parse_args()
+    if not arguments.interface:
+        print("[-] Please specify an interface using -i <interface>")
+        exit()
     return arguments.interface
 
 def sniffer(iface):
@@ -13,14 +16,14 @@ def sniffer(iface):
 
     # HTTP Packets Sniffing
 def process_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        print("[+] Http Request >> " + packet[http.HTTPRequest].Host.decode() + packet[http.HTTPRequest].Path.decode())
+    if packet.haslayer(HTTPRequest):
+        print("[+] HTTP Request >> " + packet[HTTPRequest].Host.decode(errors="ignore") + packet[HTTPRequest].Path.decode(errors="ignore"))
         if packet.haslayer(scapy.Raw):
             load = packet[scapy.Raw].load.decode(errors="ignore")
             keys = ["username", "password", "pass", "email"]
             for key in keys:
                 if key in load:
-                    print("[+] Possible password/username >> " + load)
+                    print("[+] Possible credentials found >> " + load)
                     break
     
     # TCP Packets Sniffing
